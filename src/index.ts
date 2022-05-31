5f04cf41a47f7acc0fb556ff7c61cd674e4a8bb6
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-05-30 22:59:40 -0300

    cambio en If,funciona el ejercicio

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,24 @@
 //270 personas, mayores de 18 y menores de 40,solicita cuanto menores de 21, cuantas mayores
 //de 21 y cuantas personas en total
 
-let totalPersonas : number[] = new Array(270);
-let indice: number; 
+let totalPersonas: number[] = new Array(270);
+let indice: number;
 // let personas : number;
-let menores : number = 0;
-let mayores : number = 0;
+let menores: number = 0;
+let mayores: number = 0;
 
 //presento la función random para que ingrese los valores aleatorios  que luego seran definidos los minimos y los maximos
-function personasRandom (min : number,max: number) : number {
-  return Math.floor (Math.random() * (max - min) + min);
+function personasRandom(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
 }
-for (indice= 0; indice < 270; indice++); {
-  totalPersonas[indice] = personasRandom (19,40);
-  if (totalPersonas[indice] => 18 && totalPersonas[indice] <= 21) {
-    console.log("El numero de personas menores de 21 es " + menores);
- } else (totalPersonas[indice] > 21  && totalPersonas[indice] <= 40) {
-   console.log("El numero de personas mayores de 21 es " + mayores);
-  };
-};
-
-
-
-
-
-
-
-
- 
-
-
-
-
+for (indice = 0; indice < 270; indice++) {
+  totalPersonas[indice] = personasRandom(19, 40);
+  if (totalPersonas[indice] >= 18 && totalPersonas[indice] <= 21) {
+    menores++;
+  } else if (totalPersonas[indice] > 21 && totalPersonas[indice] <= 40) {
+    mayores++;
+  }
+}
+console.log("El numero de personas menores de 21 es " + menores);
+console.log("El numero de personas mayores de 21 es " + mayores);
+console.log("El numero total de personas es", menores + mayores);
